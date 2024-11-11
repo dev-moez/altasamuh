0b2198f4bf7a6e9e9108e52a3d68c07b68a60da8
Moez <moez@career-180.com>
2024-11-11 21:38:52 +0100

    chore: Refactor ProjectDonationActions.php to handle showPhoneNumber as boolean and require authentication for phone number validation

--- app/Livewire/Projects/ProjectDonationActions.php
+++ app/Livewire/Projects/ProjectDonationActions.php
@@ -49,7 +49,7 @@ class ProjectDonationActions extends Component
             'amount' => 'required|numeric|min:' . $this->minimumDonationValue . '|max:' . $this->remainingAmount,
         ];
 
-        if (($this->project->requires_donator_phone_number || $this->showPhoneNumber == true) && auth()->check()) {
+        if ($this->project->requires_donator_phone_number && $this->showPhoneNumber == true && auth()->check()) {
             $rules['phone_number'] = 'required';
         }
 

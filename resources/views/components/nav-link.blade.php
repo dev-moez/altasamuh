96a4a1e37ff21c1afb5fd3a0ff3f7d6879ca9c20
Moez <moez@career-180.com>
2024-11-15 14:01:41 +0100

    Update navigation button styling for better accessibility

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,7 +1,7 @@
 @props(['active'])
 
 @php
-    $classes = $active ?? false ? 'inline-flex items-center px-1 pb-2 pt-1  text-md font-bold leading-5 text-blue-600 dark:text-gray-100 focus:outline-none  transition duration-150 ease-in-out' : 'inline-flex items-center px-1 pb-2 pt-1  text-md font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300   focus:outline-none focus:text-gray-700 dark:focus:text-gray-300   transition duration-150 ease-in-out';
+    $classes = $active ?? false ? 'inline-flex items-center px-1 pb-2 pt-1  text-md font-bold leading-5 text-blue-600 dark:text-gray-100 focus:outline-none  transition duration-150 ease-in-out' : 'inline-flex items-center px-1 pb-2 pt-1  text-md font-bold leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300   focus:outline-none focus:text-gray-700 dark:focus:text-gray-300   transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>

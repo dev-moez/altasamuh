6d06de2303c562aaafd80a3d6e8daf894ce09c39
Moez <moez@career-180.com>
2024-11-16 10:43:14 +0100

    Update section-header.blade.php with improved styling and layout

--- resources/views/components/section-header.blade.php
+++ resources/views/components/section-header.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="container text-center">
+    <div class="text-center max-w-[80%]">
         <h2 {{ $attributes->merge(['class' => "inline-flex relative text-center text-2xl font-bold my-8 before:content-[''] after:content-[''] before:absolute after:absolute before:top-1/2 after:top-1/2 before:w-[45px] after:w-[45px] before:h-[2px] after:h-[2px] before:bg-[#1F9E46] after:bg-[#1F9E46] before:-left-[60px] after:-right-[60px]"]) }}>
             {{ $slot ?? '' }}
         </h2>

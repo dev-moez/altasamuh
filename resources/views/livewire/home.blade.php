fcffee66dd6dd48715630197047c823b04c04895
Moez <moez@career-180.com>
2024-11-22 19:24:00 +0100

    Improve image size and styling in view-project.blade.php

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -77,8 +77,8 @@
         </div>
     </section>
     @if ($articles->count() > 0)
-        <section class="pb-12 bg-top bg-no-repeat bg-cover" style="background-image: url('{{ asset('images/articles-bg.png') }}')">
-            <div class="container mt-5">
+        <section class="pb-12 mt-5 bg-top bg-no-repeat bg-cover" style="background-image: url('{{ asset('images/articles-bg.png') }}')">
+            <div class="container">
                 <x-section-header class="text-blue-900">
                     أخبــار الجمــعيــة
                 </x-section-header>
